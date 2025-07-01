5929e6f5aa5c8f3a8ab04808d6de59940c6d2849
andrinr <andrinrehmann@gmail.com>
2025-07-01 15:29:10 +0200

    kd tree, intersection, colors, angles

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+use geo::Distance;
 use macroquad::prelude::*;
 use ndarray::{Array2, Array1};
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::rand;
+use kdtree::KdTree;
+use kdtree::ErrorKind;
+use kdtree::distance::squared_euclidean;
+use geo::{Line, Euclidean, Point};
 
 struct MLP {
     weights: Array2<f32>,
     biases: Array1<f32>,
+}
+
+fn init_mlp(input_size: usize, output_size: usize, scale : f32) -> MLP {
+    MLP {
+        weights: Array2::random(
+            (output_size, input_size),
+             Uniform::new(-scale, scale)
+        ),
+        biases: Array1::random(
+            output_size, 
+            Uniform::new(-scale, scale)
+        )
+    }
 }
 
 struct Brain {
@@ -13,25 +34,61 @@ struct Brain {
 }
 
 struct Organism {
+    id : usize,
     pos: Array1<f32>,
     vel: Array1<f32>,
     rot: f32,
     energy: f32,
-    signal: Vec3,
-    memory: Vec<f32>,
+    signal: Array1<f32>,
+    memory: Array1<f32>,
     brain: Brain,
 }
 
-fn think(brain : &Brain, inputs: Vec<f32>) -> Vec<f32> {
+fn vision_vectors(organism: &Organism) -> Vec<Array1<f32>> {
+    let vision_length = 30.0;
+    let angles = [0.0, 45.0, -45.0]; // angles in degrees
+    let mut vectors = Vec::new();
 
-    let mut output = vec![0.0; brain.output.weights.len()];
-    
-    for (i, weights) in brain.output.weights.iter().enumerate() {
-        output[i] = weights.iter().zip(&inputs).map(|(w, i)| w * i).sum::<f32>() + brain.output.biases[i];
+    for &angle in angles.iter() {
+        let angle_rad = (organism.rot + angle).to_radians();
+        let vision_vector = Array1::from_vec(vec![
+            angle_rad.cos() as f32 * vision_length,
+            angle_rad.sin() as f32 * vision_length,
+        ]);
+        vectors.push(vision_vector);
     }
+    vectors
+}
+
+fn line_circle_intersect(
+    line_start: &Array1<f32>,
+    line_end: &Array1<f32>,
+    circle_center: &Array1<f32>,
+    circle_radius: f32,
+) -> bool {
+    let p = Point::new(circle_center[0], circle_center[1]);
+    let line = Line::new(
+        Point::new(line_start[0], line_start[1]),
+        Point::new(line_end[0] , line_end[1]),
+    );
+    Euclidean.distance(&p, &line) <= circle_radius
+}
     
-    // Apply activation function (e.g., ReLU)
-    output.iter_mut().for_each(|x| *x = x.max(0.0));
+
+fn think(brain : &Brain, inputs: Array1<f32>) -> Array1<f32> {
+
+    let mut output = Array1::zeros(brain.output.weights.shape()[0]);
+
+    // Embedding layer
+    let embedded = brain.embedd.weights.dot(&inputs) + &brain.embedd.biases;
+    let embedded = embedded.map(|x| x.tanh()); // Activation function for embedding
+    // Hidden layer
+    let hidden = brain.hidden.weights.dot(&embedded) + &brain.hidden.biases;
+    let hidden = hidden.map(|x| x.tanh()); // Activation function for hidden layer
+
+    // Output layer
+    output = brain.output.weights.dot(&hidden) + &brain.output.biases;
+    output = output.map(|x| x.tanh()); // Activation function for output layer
 
     output
 }
@@ -56,7 +113,11 @@ fn wrap_around(v: &Vec2) -> Vec2 {
 #[macroquad::main("Evolutionary Organisms")]
 async fn main() {
 
+    let mut rng = rand::thread_rng();
+
     let mut organisms = Vec::new();
+
+    let mut kdtree = KdTree::new(2);
 
     let organism_count = 10;
     let init_velocity = 200.;
@@ -74,7 +135,10 @@ async fn main() {
             let text = "Start a new evolution by pressing Enter";
             let font_size = 30.;
 
-            screen_center = Vec2::new(screen_width() / 2., screen_height() / 2.);
+            screen_center = Array1::from_vec(vec![
+                screen_width() / 2.,
+                screen_height() / 2.,
+            ]);
 
             let text_size = measure_text(text, None, font_size as _, 1.0);
             draw_text(
@@ -84,54 +148,78 @@ async fn main() {
                 font_size,
                 DARKGRAY,
             );
+
             if is_key_down(KeyCode::Enter) {
 
                 genesis = false;
 
-                print!("genesis done");
-                
-                for _ in 0.. organism_count {
+                for i in 0.. organism_count {
 
-                    organisms.push(Organism {
-                        pos: screen_center
-                            + Vec2::new(rand::gen_range(-1., 1.), rand::gen_range(-1., 1.))
-                                .normalize()
-                                * screen_width().min(screen_height())
-                                / 2.,
-                        vel: Vec2::new(rand::gen_range(init_velocity * -1., init_velocity),
-                                       rand::gen_range(init_velocity * -1., init_velocity)),
-                        rot: 0.,
+                    let organism = Organism {
+                        id: i,
+                        pos: &screen_center + Array1::random(2, Uniform::new(-100., 100.)),
+                        vel: Array1::random(2, Uniform::new(-init_velocity, init_velocity)),
+                        // random rotation in radians
+                        rot : rand::random::<f32>() * std::f32::consts::PI * 2.,
                         energy: 1.,
-                        signal: Vec3::new(0., 0., 0.),
-                        memory: vec![0.; 10],
+                        signal: Array1::random(3, Uniform::new(0., 1.)),
+                        memory: Array1::zeros(3),
                         brain: Brain {
-                            embedd: MLP {
-                                weights: vec![vec![0.; 10]; 10],
-                                biases: vec![0.; 10],
-                            },
-                            hidden: MLP {
-                                weights: vec![vec![0.; 10]; 10],
-                                biases: vec![0.; 10],
-                            },
-                            output: MLP {
-                                weights: vec![vec![0.; 3]; 10],
-                                biases: vec![0.; 3],
-                            },
+                            embedd: init_mlp(10, 10, 0.1),
+                            hidden: init_mlp(10, 10, 0.1),
+                            output: init_mlp(10, 3, 0.1),
                         },
-                    });
+                    };
+
+                    kdtree.add(
+                        organism.pos.to_vec(),
+                        i as usize,
+                    ).unwrap();
+
+                    organisms.push(organism);
+
                 }
             }
             next_frame().await;
             continue;
         }
-
-        // euler integration of eac organism
+        
         for organism in organisms.iter_mut() {
-            println!("Organism velocity: {:?}", organism.vel);
-            organism.pos += organism.vel * get_frame_time();
-            organism.pos = wrap_around(&organism.pos);
+
+            
+            // euler integration
+            organism.pos += &(&organism.vel * get_frame_time());
             organism.vel *= 0.99; // friction
             organism.energy -= 0.01; // energy consumption
+
+            // get nearest neighbors
+            let neighbors = kdtree.within(
+                &organism.pos.to_vec(),
+                50.0,
+                &squared_euclidean,
+            );
+
+            let vision = vision_vectors(organism);
+
+            // // check if any of the neighbours intersects with the vision vectors
+            // for neighbor in neighbors.iter() {
+            //     let neighbour_org = &organisms[neighbor.1];
+
+            //     for vision_vector in vision.iter() {
+         
+            //         if line_circle_intersect(
+            //             &organism.pos,
+            //             &(organism.pos + vision_vector),
+            //             &neighbour_org.pos,
+            //             10.0, // radius of the organism
+            //         ) {
+            //             // if there is an intersection, we can interact with the neighbor
+            //             organism.signal[0] += 0.1; // increase signal
+            //             organism.memory[0] += 0.1; // increase memory
+            //             organism.energy += 0.1; // gain energy
+            //         }
+            //     }
+            // }
         }
 
         clear_background(LIGHTGRAY);
@@ -139,32 +227,40 @@ async fn main() {
         // draw the organisms
         for organism in organisms.iter() {
             // organism body, simple circle
-            draw_circle(organism.pos.x, organism.pos.y, 10., DARKBLUE);
-            
+            draw_circle(
+                organism.pos[0], 
+                organism.pos[1],
+                10., 
+                Color::from_rgba(
+                    (organism.signal[0] * 255.0) as u8, 
+                    (organism.signal[1] * 255.0) as u8, 
+                    (organism.signal[2] * 255.0) as u8, 
+                    255)
+            );
+                
             // visualize vision vectors by drawing dotted line for each direction
             let vision_length = 30.0;
-            let angles = [0.0, 45.0, -45.0]; // angles in degrees
+            let angles = [0.0, std::f32::consts::PI / 4.0, -std::f32::consts::PI / 4.0]; // angles in radians
             let colors = [BLACK, RED, BLUE];
 
             for (i, &angle) in angles.iter().enumerate() {
-                let angle_rad = (organism.rot + angle).to_radians();
-                let vision_vector = Vec2::new(angle_rad.cos(), angle_rad.sin()) * vision_length;
-                let end_point = organism.pos + vision_vector;
+                let angle_rad = organism.rot + angle;
+                // let vision_vector = Vec2::new(angle_rad.cos(), angle_rad.sin()) * vision_length;
+                let vision_vector = Array1::from_vec(vec![
+                    angle_rad.cos() as f32 * vision_length,
+                    angle_rad.sin() as f32 * vision_length,
+                ]);
+                let end_point = &organism.pos + vision_vector;
 
-                // Draw the dotted line
-                let mut current_point = organism.pos;
-                let step = 10.0; // length of each segment
-                while (current_point - end_point).length() > step {
-                    draw_line(
-                        current_point.x,
-                        current_point.y,
-                        (current_point + vision_vector.normalize() * step).x,
-                        (current_point + vision_vector.normalize() * step).y,
-                        2.0,
-                        colors[i],
-                    );
-                    current_point += vision_vector.normalize() * step;
-                }
+                draw_line(
+                    organism.pos[0],
+                    organism.pos[1],
+                    end_point[0],
+                    end_point[1],
+                    2.0,
+                    colors[i],
+                );
+              
             }
         }
 
